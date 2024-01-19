bdf16e5e382b3ccd68bf7bd244f9c7ff3320bde5
Confidaunt <confidaunte@gmail.com>
2024-01-18 20:00:56 -0500

    cleaning up the unneeded stuff and adding a thanks

--- quartz/components/Properties.tsx
+++ quartz/components/Properties.tsx
@@ -1,13 +1,8 @@
-import { FullSlug, _stripSlashes, joinSegments, pathToRoot, slugifyFilePath, TransformOptions, transformInternalLink, transformLink, resolveRelative, simplifySlug, FilePath } from "../util/path"
+import { FullSlug, _stripSlashes, TransformOptions, transformLink} from "../util/path"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import  style from "./styles/properties.scss"
-import { URL } from "url"
-import { hostname } from "os"
-import { range } from "d3"
-import { JSXInternal } from "preact/src/jsx"
 
 function createLinkedElement(fileData: any, opts : any, value: string) {
- 
   let cleanedValue = value.replace(/['"\[\]]+/g, '')
   let href = transformLink(fileData.slug!, cleanedValue, opts)
 
@@ -17,18 +12,11 @@ function createLinkedElement(fileData: any, opts : any, value: string) {
 }
 
 function createPropertyElement(key: string, value: any) {
-  if (value != "") {
-    return(
-      <li>
-        {key} : {value}
-      </li>
-    )
-  }
-  else {
-    return (
-      <li>{key} : None</li>
-    )
-  }
+  return(
+    <li>
+      {key} : {value}
+    </li>
+  )
 }
 
 export default (() => {
@@ -66,68 +54,12 @@ export default (() => {
       </div>
     )
 
-
-/*
-    var linkedElements = [] 
-    const valueArray = fileData.frontmatter?.Allies
-
-    for (const [index, arrayItem] of Object.entries(valueArray ?? {})) {
-      var entry = valueArray[index]
-      if(entry.includes("[[")){
-        if(Number(index) > 0){
-          linkedElements.push(", ")
-        }
-        linkedElements.push(createLinkedElement(fileData, opts, entry))
-      }
-      else{
-        linkedElements.push(entry)
-      }
-    }
-
-    return (      
-      <div class="properties">
-        <p>{linkedElements}</p>
-      </div>
-    ) 
-    */
-    //var valueArray = value?.split(",")
-    //console.log(valueArray)
-    //linkedElements.push(createPropertyElement(fileData, valueArray, opts))
-
-
-    //return (      
-    //  <div class="properties">
-    //    <ul>{linkedElements}</ul>
-    //  </div>
-    // )
-
-   // let entry = ""
-   // let href = ""
-    //console.log(value.length)
-    //if (value){
-    //  for (var i of range(value.length)) {
-    //    console.log(value[i])
-    //    entry = value[i]
-    //    entry = entry.replace(/['"\[\]]+/g, '')
-    //    href = transformLink(fileData.slug!, value, opts)
-
-    //  }
-    //}
-
-   // console.log(value)
-    //value = value.replace(/['"\[\]]+/g, '')
-    //let href = transformLink(fileData.slug!, value, opts)
-    //return (
-    //  <p><a  href={href} class="internal">{value}</a></p>
-    //)
-        //<ul>{linkedElements}</ul>
-
-
-
   }
-        
+
     PropertiesWithWorkingLinks.css = style
     return PropertiesWithWorkingLinks
 
   }
-  ) satisfies QuartzComponentConstructor
+) satisfies QuartzComponentConstructor
+
+/* Credit to both https://github.com/gamberoillecito for their author and nextnote from which I built this and to https://github.com/jackyzha0 both the creator and gracious question answerer*/
